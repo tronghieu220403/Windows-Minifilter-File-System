c42e81fa157f49f839c97e10f7ac0ad968ccbb62
Nguyễn Trọng Hiếu <tronghieu220403@gmail.com>
2024-09-12 10:05:48 +0700

    Add object callback interface

--- driver/function/protect/proc-protect.cpp
+++ driver/function/protect/proc-protect.cpp
@@ -15,10 +15,39 @@ namespace protect_proc
 		{
 			DebugMessage("Fail to register: %x", status);
 		}
+
+		OB_CALLBACK_REGISTRATION obRegistration = { 0, };
+		OB_OPERATION_REGISTRATION opRegistration[2] = {};
+
+		obRegistration.Version = ObGetFilterVersion();	// Get version
+		obRegistration.OperationRegistrationCount = 2;
+		RtlInitUnicodeString(&obRegistration.Altitude, L"2204");
+		obRegistration.RegistrationContext = NULL;
+
+		opRegistration[0].ObjectType = PsProcessType;
+		opRegistration[0].Operations = OB_OPERATION_HANDLE_CREATE;
+		opRegistration[0].PreOperation = PreObCallback;
+		opRegistration[0].PostOperation = nullptr;
+
+		opRegistration[1].ObjectType = PsThreadType;
+		opRegistration[1].Operations = OB_OPERATION_HANDLE_CREATE;
+		opRegistration[1].PreOperation = PreObCallback;
+		opRegistration[1].PostOperation = nullptr;
+
+		obRegistration.OperationRegistration = opRegistration;
+
+		status = ObRegisterCallbacks(&obRegistration, &kHandleRegistration);
+		if (STATUS_SUCCESS != status)
+		{
+			DebugMessage("Fail to register ObRegisterCallbacks: %x", status);
+		}
+
 	}
 	void DrvUnload()
 	{
 		PsSetCreateProcessNotifyRoutineEx((PCREATE_PROCESS_NOTIFY_ROUTINE_EX)&protect_proc::ProcessNotifyCallBack, TRUE);
+		ObUnRegisterCallbacks(kHandleRegistration);
+
 		delete kProtectProcIdList;
 		delete kProtectProcImageList;
 
@@ -26,25 +55,28 @@ namespace protect_proc
 
 	void ProcessNotifyCallBack(PEPROCESS eprocess, size_t pid, PPS_CREATE_NOTIFY_INFO create_info)
 	{
+		if (kEnableProtectProc == false)
+		{
+			return;
+		}
+
 		if (create_info) // Process creation
 		{
-			if (create_info->ImageFileName == nullptr || create_info->FileOpenNameAvailable == FALSE)
-			{
-				return;
-			}
-
-			String<WCHAR> process_image_name(*(create_info)->ImageFileName);
-			if (String<WCHAR>(L"\\??\\").IsPrefixOf(process_image_name))
-			{
-				process_image_name = &process_image_name[String<WCHAR>(L"\\??\\").Size()];
-			}
-
-			
+			String<WCHAR> process_image_name(process::GetProcessImageName(pid));
 		}
 		else // Process termination
 		{
 
 		}
+	}
+
+	OB_PREOP_CALLBACK_STATUS PreObCallback(PVOID RegistrationContext, POB_PRE_OPERATION_INFORMATION pOperationInformation)
+	{
+		if (kEnableProtectProc == false)
+		{
+			return OB_PREOP_CALLBACK_STATUS();
+		}
+		return OB_PREOP_CALLBACK_STATUS();
 	}
 }
 
